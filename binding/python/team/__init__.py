83b258c61a3551a2e0ccf6e063ceb6cd00cf007f
Jiří Župka <jzupka@redhat.com>
2011-10-19 11:17:26 +0200

    Change way how to work with python team module.
    example:
      import team
      t = team.team(team_name)
    
    Signed-off-by: Jiří Župka <jzupka@redhat.com>

--- binding/python/team/__init__.py
+++ binding/python/team/__init__.py
@@ -1,1 +1,1 @@
-import core
+from core import *
--- binding/python/team/core.py
+++ binding/python/team/core.py
@@ -1,1 +1,8 @@
 import capi
+
+class team:
+    def __init__(self,team_name):
+        self.th = capi.team_alloc()
+        err = capi.team_init(self.th, team_name)
+        if err:
+            raise Exception("Team init failed.")
