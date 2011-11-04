de41640cb5527d9ddde8adc3526aec8eb3eaa13d
Jiri Pirko <jpirko@redhat.com>
2011-11-04 12:21:50 +0100

    reorder team.h into groups
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>

--- include/team.h
+++ include/team.h
@@ -17,7 +17,48 @@
 extern "C" {
 #endif
 
+/*
+ * team_handle
+ *
+ * library user context
+ */
 struct team_handle;
+
+struct team_handle *team_alloc(void);
+int team_init(struct team_handle *th, uint32_t ifindex);
+void team_free(struct team_handle *th);
+int team_get_event_fd(struct team_handle *th);
+void team_process_event(struct team_handle *th);
+void team_check_events(struct team_handle *th);
+int team_get_mode_name(struct team_handle *th, char **mode_name);
+int team_set_mode_name(struct team_handle *th, char *mode_name);
+int team_get_active_port(struct team_handle *th, uint32_t *ifindex);
+int team_set_active_port(struct team_handle *th, uint32_t ifindex);
+
+/*
+ * team_port
+ *
+ * access to port_list and individual port
+ */
+struct team_port;
+
+struct team_port *team_get_next_port(struct team_handle *th,
+				     struct team_port *port);
+#define team_for_each_port(port, th)				\
+	for (port = team_get_next_port(th, NULL); port;		\
+	     port = team_get_next_port(th, port))
+/* port getters */
+uint32_t team_get_port_ifindex(struct team_port *port);
+uint32_t team_get_port_speed(struct team_port *port);
+uint8_t team_get_port_duplex(struct team_port *port);
+bool team_is_port_changed(struct team_port *port);
+bool team_is_port_link_up(struct team_port *port);
+
+/*
+ * team_option
+ *
+ * access to option_list and individual option
+ */
 struct team_port;
 
 enum team_option_type {
@@ -27,14 +68,33 @@ enum team_option_type {
 
 struct team_option;
 
-#define team_for_each_port(port, th)				\
-	for (port = team_get_next_port(th, NULL); port;		\
-	     port = team_get_next_port(th, port))
-
+struct team_option *team_get_option_by_name(struct team_handle *th, char *name);
+struct team_option *team_get_next_option(struct team_handle *th,
+					 struct team_option *option);
 #define team_for_each_option(port, th)				\
 	for (option = team_get_next_option(th, NULL); option;	\
 	     option = team_get_next_option(th, option))
+/* option getters */
+char *team_get_option_name(struct team_option *option);
+enum team_option_type team_get_option_type(struct team_option *option);
+bool team_is_option_changed(struct team_option *option);
+uint32_t team_get_option_value_u32(struct team_option *option);
+char *team_get_option_value_string(struct team_option *option);
+int team_get_option_value_by_name_u32(struct team_handle *th,
+				      char *name, uint32_t *u32_ptr);
+int team_get_option_value_by_name_string(struct team_handle *th,
+					 char *name, char **str_ptr);
+/* option setters */
+int team_set_option_value_by_name_u32(struct team_handle *th,
+				      char *opt_name, uint32_t val);
+int team_set_option_value_by_name_string(struct team_handle *th,
+					 char *opt_name, char *str);
 
+/*
+ * team_change_handler
+ *
+ * define and register change handler frunctions
+ */
 enum team_change_type {
 	TEAM_ALL_CHANGE,
 	TEAM_PORT_CHANGE,
@@ -47,50 +107,14 @@ struct team_change_handler {
 	enum team_change_type	type;
 };
 
-struct team_handle *team_alloc(void);
-int team_init(struct team_handle *th, uint32_t ifindex);
-void team_free(struct team_handle *th);
-int team_get_event_fd(struct team_handle *th);
-void team_process_event(struct team_handle *th);
-void team_check_events(struct team_handle *th);
-struct team_port *team_get_next_port(struct team_handle *th,
-				     struct team_port *port);
-struct team_option *team_get_next_option(struct team_handle *th,
-					 struct team_option *option);
 int team_change_handler_register(struct team_handle *th,
 				 struct team_change_handler *handler);
 void team_change_handler_unregister(struct team_handle *th,
 				    struct team_change_handler *handler);
 
-/* port getters */
-uint32_t team_get_port_ifindex(struct team_port *port);
-uint32_t team_get_port_speed(struct team_port *port);
-uint8_t team_get_port_duplex(struct team_port *port);
-bool team_is_port_changed(struct team_port *port);
-bool team_is_port_link_up(struct team_port *port);
-
-/* option getters/setters */
-char *team_get_option_name(struct team_option *option);
-enum team_option_type team_get_option_type(struct team_option *option);
-bool team_is_option_changed(struct team_option *option);
-struct team_option *team_get_option_by_name(struct team_handle *th, char *name);
-uint32_t team_get_option_value_u32(struct team_option *option);
-char *team_get_option_value_string(struct team_option *option);
-int team_get_option_value_by_name_u32(struct team_handle *th,
-				      char *name, uint32_t *u32_ptr);
-int team_get_option_value_by_name_string(struct team_handle *th,
-					 char *name, char **str_ptr);
-int team_set_option_value_by_name_u32(struct team_handle *th,
-				      char *opt_name, uint32_t val);
-int team_set_option_value_by_name_string(struct team_handle *th,
-					 char *opt_name, char *str);
-
-int team_get_mode_name(struct team_handle *th, char **mode_name);
-int team_set_mode_name(struct team_handle *th, char *mode_name);
-int team_get_active_port(struct team_handle *th, uint32_t *ifindex);
-int team_set_active_port(struct team_handle *th, uint32_t ifindex);
-
-/* Route netlink helper function */
+/*
+ * route netlink helper functions
+ */
 uint32_t team_ifname2ifindex(struct team_handle *th, const char *ifname);
 char *team_ifindex2ifname(struct team_handle *th, uint32_t ifindex,
 			  char *ifname, unsigned int maxlen);
