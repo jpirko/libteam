b99eb9ff7a206fe8518051376f4b965f1a17624d
Jiri Pirko <jpirko@redhat.com>
2011-11-25 13:01:00 +0100

    team_daemon.py: convert app core to be in class
    also remove active-port option because it has no sense
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>

--- examples/python/team_daemon.py
+++ examples/python/team_daemon.py
@@ -30,132 +30,136 @@ def usage():
     print "  -t, --team-name=NAME               name of team device"
     print "  -p, --port=NETDEV                  port device (can be defined multiple times)"
     print "  -m, --mode=MODENAME                name of team mode"
-    print "  -a, --active-port=NETDEV           initial active port"
     sys.exit()
 
+class TeamDaemon(object):
+    def __init__(self):
+        def_team_name = "teamtest0"
+        self._team_name = def_team_name
+        def_mode_name = "roundrobin"
+        self._mode_name = def_mode_name
+        self._port_names = []
+        self._old_active_hwaddr = None
 
-def port_change_handler_func(t):
-    print "------------------\nport change\n\tport list:"
-    for port in t.port_list():
-        print ("\tifname %s, linkup %d, changed %d, speed %d, duplex %d" %
+        self._parse_cmdline()
+
+        if self._team_name == def_team_name:
+            print "Using default team name \"%s\"" % def_team_name
+
+        if self._mode_name == def_mode_name:
+            print "Using default mode \"%s\"" % def_mode_name
+
+        print "Team name is \"%s\"" % self._team_name
+        self._t = team.Team(self._team_name, create=True, destroy=True)
+
+        self._t.set_mode_name(self._mode_name)
+        for port_name in self._port_names:
+            self._t.port_add(port_name)
+
+        if self._mode_name == "activebackup":
+            self._port_change_handler_ab = team.TeamChangeHandler(
+                                    self._port_change_handler_ab_func, None,
+                                    team.TEAM_PORT_CHANGE)
+            self._t.change_handler_register(self._port_change_handler_ab)
+
+        self._port_change_handler = team.TeamChangeHandler(
+                                    self._port_change_handler_func, None,
+                                    team.TEAM_PORT_CHANGE)
+        self._t.change_handler_register(self._port_change_handler)
+
+        self._option_change_handler = team.TeamChangeHandler(
+                                    self._option_change_handler_func, None,
+                                    team.TEAM_OPTION_CHANGE)
+        self._t.change_handler_register(self._option_change_handler)
+
+    def _parse_cmdline(self):
+        try:
+            opts, args = getopt.getopt(
+                sys.argv[1:],
+                "ht:p:m:",
+                ["help", "team-name=", "port=", "mode="]
+            )
+        except getopt.GetoptError, err:
+            print str(err)
+            usage()
+
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                usage()
+            elif opt in ("-t", "--team-name"):
+                self._team_name = arg
+            elif opt in ("-p", "--port"):
+                self._port_names.append(arg)
+            elif opt in ("-m", "--mode"):
+                self._mode_name = arg
+
+    def _port_change_handler_func(self, unused):
+        print "------------------\nport change\n\tport list:"
+        for port in self._t.port_list():
+            print ("\tifname %s, linkup %d, changed %d, speed %d, duplex %d" %
                         (port.ifname, port.linkup, port.changed,
                          port.speed, port.duplex))
 
-def option_change_handler_func(t):
-    print "------------------\noption change\n\toption list:"
-    for option in t.option_list():
-        print ("\t%s = %s (changed %d)" % (option.name, option.value,
-                                           option.changed))
+    def _option_change_handler_func(self, unused):
+        print "------------------\noption change\n\toption list:"
+        for option in self._t.option_list():
+            print ("\t%s = %s (changed %d)" % (option.name, option.value,
+                                               option.changed))
 
-orig_active_hwaddr = None
+    def _change_active_port(self, old_active, new_active):
+        if old_active:
+            old_active.set_hwaddr(self._old_active_hwaddr)
 
-def change_active_port(t, old_active, new_active):
-    global orig_active_hwaddr
-    if old_active:
-        old_active.set_hwaddr(orig_active_hwaddr)
+        self._t.set_active_port(new_active)
+        mac = self._t.get_hwaddr()
+        self._old_active_hwaddr = new_active.get_hwaddr()
+        new_active.set_hwaddr(mac)
+        print "*** new active port \"%s\" selected" % new_active
 
-    t.set_active_port(new_active)
-    mac = t.get_hwaddr()
-    orig_active_hwaddr = new_active.get_hwaddr()
-    new_active.set_hwaddr(mac)
-    print "*** new active port \"%s\" selected" % new_active
+    def _port_change_handler_ab_func(self, unused):
+        try:
+            active = self._t.get_active_port()
+            if active:
+                print "*** current active port is \"%s\"" % active
+                if active.linkup:
+                    return
+                elif active.changed:
+                    print "*** active port \"%s\" lost link" % active
+        except KeyError:
+            active = None
+            print "*** no current active port set"
 
-def port_change_handler_ab_func(t):
-    active = t.get_active_port()
-    print "*** current active port is \"%s\"" % active
-    if active.linkup:
-        return
-    elif active.changed:
-        print "*** active port \"%s\" lost link" % active
+        best = None
+        for port in self._t.port_list():
+            if port.linkup:
+                if (not best or
+                    port.speed > best.speed or
+                    (port.speed == best.speed and port.duplex > port.duplex)):
+                    best = port
+        if best:
+            self._change_active_port(active, best)
 
-    best = None
-    for port in t.port_list():
-        if port.linkup:
-            if (not best or
-                port.speed > best.speed or
-                (port.speed == best.speed and port.duplex > port.duplex)):
-                best = port
-    if best:
-        change_active_port(t, active, best)
+    def close(self):
+        self._t.close()
+
+    def loop_forever(self):
+        self._t.loop_forever()
+
+    def kill_loop(self):
+        self._t.kill_loop()
 
 def main():
-    try:
-        opts, args = getopt.getopt(
-            sys.argv[1:],
-            "ht:p:m:a:",
-            ["help", "team-name=", "port=", "mode=", "active-port="]
-        )
-    except getopt.GetoptError, err:
-        print str(err)
-        usage()
-
-    team_name = None
-    mode_name = None
-    active_port_name = None
-    ports = []
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-        elif opt in ("-t", "--team-name"):
-            team_name = arg
-        elif opt in ("-p", "--port"):
-            ports.append(arg)
-        elif opt in ("-m", "--mode"):
-            mode_name = arg
-        elif opt in ("-a", "--active-port"):
-            active_port_name = arg
-
-    appname = sys.argv[0]
-    args = sys.argv[1:]
-
-    if not team_name:
-        def_team_name = "teamtest0"
-        print "Using default team name \"%s\"" % def_team_name
-        team_name = def_team_name
-    if not mode_name:
-        def_mode_name = "roundrobin"
-        print "Using default mode \"%s\"" % def_mode_name
-        mode_name = def_mode_name
-
-    print "team name is \"%s\"" % team_name
-    t = team.Team(team_name, create=True, destroy=True)
-    t.set_mode_name(mode_name)
-    for port in ports:
-        t.port_add(port)
-
-    t.check_events()
-
-    port_change_handler = team.TeamChangeHandler(
-                                    port_change_handler_func, t,
-                                    team.TEAM_PORT_CHANGE)
-    t.change_handler_register(port_change_handler)
-
-    option_change_handler = team.TeamChangeHandler(
-                                    option_change_handler_func, t,
-                                    team.TEAM_OPTION_CHANGE)
-    t.change_handler_register(option_change_handler)
-
-    if mode_name == "activebackup":
-        if not active_port_name:
-            if not ports:
-                usage()
-            active_port_name = ports[0]
-        port = t.get_port(active_port_name)
-        change_active_port(t, None, port)
-        port_change_handler_ab = team.TeamChangeHandler(
-                                    port_change_handler_ab_func, t,
-                                    team.TEAM_PORT_CHANGE)
-        t.change_handler_register(port_change_handler_ab)
+    team_daemon = TeamDaemon()
 
     def kill_loop(signum, frame):
-        t.kill_loop()
+        team_daemon.kill_loop()
 
     signal.signal(signal.SIGINT, kill_loop)
     signal.signal(signal.SIGHUP, kill_loop)
     signal.signal(signal.SIGTERM, kill_loop)
 
-    t.loop_forever()
-    t.close()
+    team_daemon.loop_forever()
+    team_daemon.close()
 
 if __name__ == "__main__":
     main()
