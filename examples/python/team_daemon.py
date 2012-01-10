e85b2b9e037d90f1005b09f298f75bacadbfbc2b
Jiri Pirko <jpirko@redhat.com>
2012-01-10 15:10:17 +0100

    licence: update COPYING file and all file headers
    Update COPYING file so it contains most recent version of licence
    (address changed). Also update all file headers so they have exact
    format as it's written in licence.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>

--- examples/python/team_daemon.py
+++ examples/python/team_daemon.py
@@ -4,11 +4,21 @@ Team daemon example. Implements very basic active backup functionality.
 Note that this application spawns its own team device instead of connecting
 to an existing one.
 
-Copyright (c) 2011 Jiri Pirko <jpirko@redhat.com>
+   Copyright (C) 2011 Jiri Pirko <jpirko@redhat.com>
 
-This library is free software; you can redistribute it and/or modify it
-under the terms of the GNU Lesser General Public License as published
-by the Free Software Foundation version 2.1 of the License.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
 __author__ = """
--- src/team_manual_control.c
+++ src/team_manual_control.c
@@ -1,10 +1,20 @@
 /*
- * team_manual_control.c - Network team device dummy manual control
- * Copyright (c) 2011 Jiri Pirko <jpirko@redhat.com>
+ *   team_manual_control.c - Network team device dummy manual control
+ *   Copyright (C) 2011 Jiri Pirko <jpirko@redhat.com>
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation version 2.1 of the License.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <stdio.h>
