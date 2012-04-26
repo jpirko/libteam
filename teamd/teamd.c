2785827664706f462aa17729ee2529f3d2d36c83
Jiri Pirko <jpirko@redhat.com>
2012-04-26 11:27:34 +0200

    teamd: fix debug_log_option_list() output
    use same format as for other debug outputs. Make ifindex be the first.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>

--- teamd/teamd.c
+++ teamd/teamd.c
@@ -968,35 +968,30 @@ static void debug_log_option_list(struct teamd_context *ctx)
 	team_for_each_option(option, ctx->th) {
 		char *name = team_get_option_name(option);
 		bool changed = team_is_option_changed(option);
-		uint32_t port_ifindex = team_get_option_port_ifindex(option);
+		uint32_t ifindex = team_get_option_port_ifindex(option);
 
 		switch (team_get_option_type(option)) {
 		case TEAM_OPTION_TYPE_U32:
-			teamd_log_dbg("%s(%d): \"%d\" <int>%s", name,
-				      port_ifindex,
+			teamd_log_dbg("%d: %s: \"%d\" <int>%s", ifindex, name,
 				      team_get_option_value_u32(option),
 				      changed ? " changed" : "");
 			break;
 		case TEAM_OPTION_TYPE_STRING:
-			teamd_log_dbg("%s(%d): \"%s\" <str>%s", name,
-				      port_ifindex,
+			teamd_log_dbg("%d: %s: \"%s\"<str>%s", ifindex, name,
 				      team_get_option_value_string(option),
 				      changed ? " changed" : "");
 			break;
 		case TEAM_OPTION_TYPE_BINARY:
-			teamd_log_dbg("%s(%d): <bin>%s", name,
-				      port_ifindex,
+			teamd_log_dbg("%d: %s: <bin>%s", ifindex, name,
 				      changed ? " changed" : "");
 			break;
 		case TEAM_OPTION_TYPE_BOOL:
-			teamd_log_dbg("%s(%d): \"%s\" <bool>%s", name,
-				      port_ifindex,
+			teamd_log_dbg("%d: %s: \"%s\" <bool>%s", ifindex, name,
 				      team_get_option_value_bool(option) ? "true" : "false",
 				      changed ? " changed" : "");
 			break;
 		default:
-			teamd_log_dbg("%s(%d): <unknown>%s", name,
-				      port_ifindex,
+			teamd_log_dbg("%d: %s: <unknown>%s", ifindex, name,
 				      changed ? " changed" : "");
 		}
 	}
