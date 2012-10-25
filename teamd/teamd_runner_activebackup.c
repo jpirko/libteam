c53a80ecc21fca08cb72f59a083aca16f0e64975
Jiri Pirko <jiri@resnulli.us>
2012-10-25 17:42:56 +0200

    teamd: ab call ab_link_watch_handler() when port is removed
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- teamd/teamd_runner_activebackup.c
+++ teamd/teamd_runner_activebackup.c
@@ -229,6 +229,12 @@ static int ab_event_watch_port_added(struct teamd_context *ctx,
 	return 0;
 }
 
+static void ab_event_watch_port_removed(struct teamd_context *ctx,
+					struct teamd_port *tdport, void *priv)
+{
+	ab_link_watch_handler(ctx);
+}
+
 static int ab_port_link_changed(struct teamd_context *ctx,
 				struct teamd_port *tdport, void *priv)
 {
@@ -243,6 +249,7 @@ static int ab_prio_option_changed(struct teamd_context *ctx,
 
 static const struct teamd_event_watch_ops ab_event_watch_ops = {
 	.port_added = ab_event_watch_port_added,
+	.port_removed = ab_event_watch_port_removed,
 	.port_link_changed = ab_port_link_changed,
 	.option_changed = ab_prio_option_changed,
 	.option_changed_match_name = "priority",
