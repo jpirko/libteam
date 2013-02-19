4aa8dd9e2569ba6bf899d906099b9c5f1e9865eb
Jiri Pirko <jiri@resnulli.us>
2013-02-19 18:16:38 +0100

    teamd: shorten "lb_priv" to "lb"
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- teamd/teamd_runner_loadbalance.c
+++ teamd/teamd_runner_loadbalance.c
@@ -23,14 +23,14 @@
 
 #include "teamd.h"
 
-struct lb_priv {
+struct lb {
 	struct teamd_balancer *tb;
 };
 
 static int lb_event_watch_port_added(struct teamd_context *ctx,
 				     struct teamd_port *tdport, void *priv)
 {
-	struct lb_priv *lb_priv = priv;
+	struct lb *lb = priv;
 	int err;
 
 	err = team_hwaddr_set(ctx->th, tdport->ifindex, ctx->hwaddr,
@@ -40,15 +40,15 @@ static int lb_event_watch_port_added(struct teamd_context *ctx,
 			      tdport->ifname);
 		return err;
 	}
-	return teamd_balancer_port_added(lb_priv->tb, tdport);
+	return teamd_balancer_port_added(lb->tb, tdport);
 }
 
 static void lb_event_watch_port_removed(struct teamd_context *ctx,
 					struct teamd_port *tdport, void *priv)
 {
-	struct lb_priv *lb_priv = priv;
+	struct lb *lb = priv;
 
-	teamd_balancer_port_removed(lb_priv->tb, tdport);
+	teamd_balancer_port_removed(lb->tb, tdport);
 }
 
 static const struct teamd_event_watch_ops lb_port_watch_ops = {
@@ -58,34 +58,34 @@ static const struct teamd_event_watch_ops lb_port_watch_ops = {
 
 static int lb_init(struct teamd_context *ctx)
 {
-	struct lb_priv *lb_priv = ctx->runner_priv;
+	struct lb *lb = ctx->runner_priv;
 	int err;
 
 	err = teamd_hash_func_set(ctx);
 	if (err)
 		return err;
-	err = teamd_event_watch_register(ctx, &lb_port_watch_ops, lb_priv);
+	err = teamd_event_watch_register(ctx, &lb_port_watch_ops, lb);
 	if (err) {
 		teamd_log_err("Failed to register event watch.");
 		return err;
 	}
-	err = teamd_balancer_init(ctx, &lb_priv->tb);
+	err = teamd_balancer_init(ctx, &lb->tb);
 	if (err) {
 		teamd_log_err("Failed to init balanced.");
 		goto event_watch_unregister;
 	}
 	return 0;
 event_watch_unregister:
-	teamd_event_watch_unregister(ctx, &lb_port_watch_ops, lb_priv);
+	teamd_event_watch_unregister(ctx, &lb_port_watch_ops, lb);
 	return err;
 }
 
 static void lb_fini(struct teamd_context *ctx)
 {
-	struct lb_priv *lb_priv = ctx->runner_priv;
+	struct lb *lb = ctx->runner_priv;
 
-	teamd_balancer_fini(lb_priv->tb);
-	teamd_event_watch_unregister(ctx, &lb_port_watch_ops, lb_priv);
+	teamd_balancer_fini(lb->tb);
+	teamd_event_watch_unregister(ctx, &lb_port_watch_ops, lb);
 }
 
 const struct teamd_runner teamd_runner_loadbalance = {
@@ -93,5 +93,5 @@ const struct teamd_runner teamd_runner_loadbalance = {
 	.team_mode_name	= "loadbalance",
 	.init		= lb_init,
 	.fini		= lb_fini,
-	.priv_size	= sizeof(struct lb_priv),
+	.priv_size	= sizeof(struct lb),
 };
