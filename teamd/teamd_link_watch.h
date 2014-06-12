fcf8eebeb97bd525f4ed473d2ec99f1480a38227
Erik Hugne <erik.hugne@ericsson.com>
2014-06-12 15:48:47 +0200

    teamd: move psr template link watcher to a separate file
    The periodic send/receive code is the parts common to the arp/icmp6
    watchers. We move all of this to teamd_lw_psr.c and declare the psr
    types/functions in the common teamd_link_watch.h.
    
    Signed-off-by: Erik Hugne <erik.hugne@ericsson.com>
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- teamd/teamd_link_watch.h
+++ teamd/teamd_link_watch.h
@@ -19,6 +19,30 @@ struct lw_common_port_priv {
 	struct teamd_config_path_cookie *cpcookie;
 };
 
+struct lw_psr_port_priv;
+
+struct lw_psr_ops {
+	int (*sock_open)(struct lw_psr_port_priv *psr_ppriv);
+	void (*sock_close)(struct lw_psr_port_priv *psr_ppriv);
+	int (*load_options)(struct teamd_context *ctx,
+			    struct teamd_port *tdport,
+			    struct lw_psr_port_priv *psr_ppriv);
+	int (*send)(struct lw_psr_port_priv *psr_ppriv);
+	int (*receive)(struct lw_psr_port_priv *psr_ppriv);
+};
+
+struct lw_psr_port_priv {
+	struct lw_common_port_priv common; /* must be first */
+	const struct lw_psr_ops *ops;
+	struct timespec interval;
+	struct timespec init_wait;
+	unsigned int missed_max;
+	int sock;
+	unsigned int missed;
+	bool reply_received;
+};
+
+
 static inline bool teamd_link_watch_link_up_differs(struct lw_common_port_priv *common_ppriv,
 					     bool new_link_up)
 {
@@ -29,5 +53,24 @@ int teamd_link_watch_check_link_up(struct teamd_context *ctx,
 				   struct teamd_port *tdport,
 				   struct lw_common_port_priv *common_ppriv,
 				   bool new_link_up);
+
+struct lw_psr_port_priv *
+lw_psr_ppriv_get(struct lw_common_port_priv *common_ppriv);
+int lw_psr_port_added(struct teamd_context *ctx, struct teamd_port *tdport,
+		      void *priv, void *creator_priv);
+void lw_psr_port_removed(struct teamd_context *ctx, struct teamd_port *tdport,
+			 void *priv, void *creator_priv);
+int lw_psr_state_interval_get(struct teamd_context *ctx,
+			      struct team_state_gsc *gsc,
+			      void *priv);
+int lw_psr_state_init_wait_get(struct teamd_context *ctx,
+			       struct team_state_gsc *gsc,
+			       void *priv);
+int lw_psr_state_missed_max_get(struct teamd_context *ctx,
+			        struct team_state_gsc *gsc,
+			        void *priv);
+int lw_psr_state_missed_get(struct teamd_context *ctx,
+			    struct team_state_gsc *gsc,
+			    void *priv);
 
 #endif
