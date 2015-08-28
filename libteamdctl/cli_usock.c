jpirko/libteam

commit 7824c18d65ab26a00b9f47dad6b8c5b6dc0930c8

libteamdctl: increase usock timeout to 5secs
Signed-off-by: Jiri Pirko <jiri@resnulli.us>

libteamdctl/cli_usock.c
@@ -77,7 +77,7 @@ static int cli_usock_send(int sock, char *msg)
 	return 0;
 }
 
-#define WAIT_USEC 500000
+#define WAIT_USEC 5000000
 
 static int cli_usock_wait_recv(int sock)
 {

commit 5b538c0ef22dd9e42513bf2c137e3efca24e807c

libteamdctl: fix timeval value for select
timeval.tv_usec shouldn't be bigger than 10^6, as then it overlaps
.tv_sec.

The fix is to normalize the fields regarding their resolutions.

Fixes: 7824c18d65ab ("libteamdctl: increase usock timeout to 5secs")
Reported-by: Vitezslav Humpa <vhumpa@redhat.com>
Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
Acked-by: Flavio Leitner <fbl@sysclose.org>
Signed-off-by: Jiri Pirko <jiri@resnulli.us>

libteamdctl/cli_usock.c
@@ -79,7 +79,8 @@ static int cli_usock_send(int sock, char *msg)
 	return 0;
 }
 
-#define WAIT_USEC (TEAMDCTL_REPLY_TIMEOUT * 1000)
+#define WAIT_SEC (TEAMDCTL_REPLY_TIMEOUT / 1000)
+#define WAIT_USEC (TEAMDCTL_REPLY_TIMEOUT % 1000 * 1000)
 
 static int cli_usock_wait_recv(int sock)
 {
@@ -88,7 +89,7 @@ static int cli_usock_wait_recv(int sock)
 	int ret;
 	struct timeval tv;
 
-	tv.tv_sec = 0;
+	tv.tv_sec = WAIT_SEC;
 	tv.tv_usec = WAIT_USEC;
 	FD_ZERO(&rfds);
 	FD_SET(sock, &rfds);