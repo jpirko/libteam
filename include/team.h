jpirko/libteam

commit 046fb6ba0aec8246075b18d787daec43201566fa

libteam: resynchronize ifinfo after lost RTNLGRP_LINK notifications
When there's a large number of interfaces (e.g. vlans), teamd loses
link notifications as it cannot read them as fast as kernel is
broadcasting them. This often prevents teamd starting properly if
started concurrently when other links are being set up. It can also
fail when it's up and running, especially in the cases where the team
device itself has a lot of vlans under it.

This can easily be reproduces by simple example (in SMP system) by
manually adding team device with a bunch of vlans, putting it up,
and starting teamd with --take-over option:

  root@debian:~# ip link add name team0 type team
  root@debian:~# for i in `seq 100 150` ; do
  > ip link add link team0 name team0.$i type vlan id $i ; done
  root@debian:~# ip link set team0 up
  root@debian:~# cat teamd.conf
  {
    "device": "team0",
    "runner": {
      "name": "activebackup"
     },
    "ports": {
      "eth1": {},
      "eth2": {}
    }
  }
  root@debian:~# teamd -o -N -f teamd.conf

At this point, teamd will not give any error messages or other
indication that something is wrong. But state will not look healthy:

  root@debian:~# teamdctl team0 state
  setup:
    runner: activebackup
  ports:
    eth1
      link watches:
        link summary: up
        instance[link_watch_0]:
          name: ethtool
          link: up
          down count: 0
  Failed to parse JSON port dump.
  command call failed (Invalid argument)

If checking state dump, it will show that port eth2 is missing info.
Running strace to teamd will reveal that there's one recvmsgs() that
returned -1 with errno ENOBUFS. What happened in this example was
that when teamd started, all vlans got carrier up, and kernel flooded
notifications faster than teamd could read them. It then lost events
related to port eth2 getting enslaved and up.

The socket that joins RTNLGRP_LINK notifications uses default libnl

libteam/ifinfo.c
@@ -72,6 +72,10 @@ struct team_ifinfo {
 #define CHANGED_PHYS_PORT_ID		(1 << 5)
 #define CHANGED_PHYS_PORT_ID_LEN	(1 << 6)
 #define CHANGED_ADMIN_STATE		(1 << 7)
+/* This is only used when tagging interfaces for finding
+ * removed, and thus not included to CHANGED_ANY.
+ */
+#define CHANGED_REFRESHING		(1 << 8)
 #define CHANGED_ANY	(CHANGED_REMOVED | CHANGED_HWADDR | \
 			 CHANGED_HWADDR_LEN | CHANGED_IFNAME | \
 			 CHANGED_MASTER_IFINDEX | CHANGED_PHYS_PORT_ID | \
@@ -202,7 +206,7 @@ static struct team_ifinfo *ifinfo_find(struct team_handle *th, uint32_t ifindex)
 	return NULL;
 }
 
-static void clear_last_changed(struct team_handle *th)
+void ifinfo_clear_changed(struct team_handle *th)
 {
 	struct team_ifinfo *ifinfo;
 
@@ -236,7 +240,7 @@ static void ifinfo_destroy(struct team_ifinfo *ifinfo)
 	free(ifinfo);
 }
 
-static void ifinfo_destroy_removed(struct team_handle *th)
+void ifinfo_destroy_removed(struct team_handle *th)
 {
 	struct team_ifinfo *ifinfo, *tmp;
 
@@ -254,8 +258,6 @@ static void obj_input_newlink(struct nl_object *obj, void *arg, bool event)
 	uint32_t ifindex;
 	int err;
 
-	ifinfo_destroy_removed(th);
-
 	link = (struct rtnl_link *) obj;
 
 	ifindex = rtnl_link_get_ifindex(link);
@@ -269,7 +271,7 @@ static void obj_input_newlink(struct nl_object *obj, void *arg, bool event)
 			return;
 	}
 
-	clear_last_changed(th);
+	clear_changed(ifinfo);
 	ifinfo_update(ifinfo, link);
 
 	if (event)
@@ -292,8 +294,6 @@ static void event_handler_obj_input_dellink(struct nl_object *obj, void *arg)
 	uint32_t ifindex;
 	int err;
 
-	ifinfo_destroy_removed(th);
-
 	link = (struct rtnl_link *) obj;
 
 	ifindex = rtnl_link_get_ifindex(link);
@@ -311,7 +311,7 @@ static void event_handler_obj_input_dellink(struct nl_object *obj, void *arg)
 		return;
 	}
 
-	clear_last_changed(th);
+	clear_changed(ifinfo);
 	set_changed(ifinfo, CHANGED_REMOVED);
 	set_call_change_handlers(th, TEAM_IFINFO_CHANGE);
 }
@@ -367,6 +367,14 @@ int get_ifinfo_list(struct team_handle *th)
 	};
 	int ret;
 	int retry = 1;
+	struct team_ifinfo *ifinfo;
+
+	/* Tag all ifinfo, this is cleared in newlink handler.
+	 * Any interface that has this after dump is processed
+	 * has been removed.
+	 */
+	list_for_each_node_entry(ifinfo, &th->ifinfo_list, list)
+		set_changed(ifinfo, CHANGED_REFRESHING);
 
 	while (retry) {
 		retry = 0;
@@ -395,6 +403,15 @@ int get_ifinfo_list(struct team_handle *th)
 			retry = 1;
 		}
 	}
+
+	list_for_each_node_entry(ifinfo, &th->ifinfo_list, list) {
+		if (is_changed(ifinfo, CHANGED_REFRESHING)) {
+			clear_changed(ifinfo);
+			set_changed(ifinfo, CHANGED_REMOVED);
+			set_call_change_handlers(th, TEAM_IFINFO_CHANGE);
+		}
+	}
+
 	ret = check_call_change_handlers(th, TEAM_IFINFO_CHANGE);
 	if (ret < 0)
 		err(th, "get_ifinfo_list: check_call_change_handers failed");

libteam/libteam.c
@@ -236,6 +236,10 @@ int check_call_change_handlers(struct team_handle *th,
 				break;
 		}
 	}
+	if (call_type_mask & TEAM_IFINFO_CHANGE) {
+		ifinfo_destroy_removed(th);
+		ifinfo_clear_changed(th);
+	}
 	th->change_handler.pending_type_mask &= ~call_type_mask;
 	return err;
 }
@@ -546,6 +550,11 @@ int team_destroy(struct team_handle *th)
 #endif
 /* \endcond */
 
+/* libnl uses default 32k socket receive buffer size,
+ * whicn can get too small. Use 96k for all sockets.
+ */
+#define NETLINK_RCVBUF 98304
+
 /**
  * @param th		libteam library context
  * @param ifindex	team device interface index
@@ -561,6 +570,8 @@ int team_init(struct team_handle *th, uint32_t ifindex)
 	int err;
 	int grp_id;
 	int val;
+	int eventbufsize;
+	const char *env;
 
 	if (!ifindex) {
 		err(th, "Passed interface index %d is not valid.", ifindex);
@@ -589,12 +600,12 @@ int team_init(struct team_handle *th, uint32_t ifindex)
 		return -errno;
 	}
 
-	err = nl_socket_set_buffer_size(th->nl_sock, 98304, 0);
+	err = nl_socket_set_buffer_size(th->nl_sock, NETLINK_RCVBUF, 0);
 	if (err) {
 		err(th, "Failed to set buffer size of netlink sock.");
 		return -nl2syserr(err);
 	}
-	err = nl_socket_set_buffer_size(th->nl_sock_event, 98304, 0);
+	err = nl_socket_set_buffer_size(th->nl_sock_event, NETLINK_RCVBUF, 0);
 	if (err) {
 		err(th, "Failed to set buffer size of netlink event sock.");
 		return -nl2syserr(err);
@@ -627,6 +638,25 @@ int team_init(struct team_handle *th, uint32_t ifindex)
 	nl_socket_modify_cb(th->nl_cli.sock_event, NL_CB_VALID,
 			    NL_CB_CUSTOM, cli_event_handler, th);
 	nl_cli_connect(th->nl_cli.sock_event, NETLINK_ROUTE);
+
+	env = getenv("TEAM_EVENT_BUFSIZE");
+	if (env) {
+		eventbufsize = strtol(env, NULL, 10);
+		/* ignore other errors, libnl forces minimum 32k and
+		 * too large values are truncated to system rmem_max
+		 */
+		if (eventbufsize < 0)
+			eventbufsize = 0;
+	} else {
+		eventbufsize = NETLINK_RCVBUF;
+	}
+
+	err = nl_socket_set_buffer_size(th->nl_cli.sock_event, eventbufsize, 0);
+	if (err) {
+		err(th, "Failed to set cli event socket buffer size.");
+		return err;
+	}
+
 	err = nl_socket_add_membership(th->nl_cli.sock_event, RTNLGRP_LINK);
 	if (err < 0) {
 		err(th, "Failed to add netlink membership.");
@@ -767,7 +797,23 @@ static int get_cli_sock_event_fd(struct team_handle *th)
 
 static int cli_sock_event_handler(struct team_handle *th)
 {
-	nl_recvmsgs_default(th->nl_cli.sock_event);
+	int err;
+
+	err = nl_recvmsgs_default(th->nl_cli.sock_event);
+	err = -nl2syserr(err);
+
+	/* libnl thinks ENOBUFS and ENOMEM are same. Hope it was ENOBUFS. */
+	if (err == -ENOMEM) {
+		warn(th, "Lost link notifications from kernel.");
+		/* There's no way to know what events were lost and no
+		 * way to get them again. Refresh all.
+		 */
+		err = get_ifinfo_list(th);
+	}
+
+	if (err)
+		return err;
+
 	return check_call_change_handlers(th, TEAM_IFINFO_CHANGE);
 }
 

libteam/team_private.h
@@ -115,6 +115,9 @@ int ifinfo_link_with_port(struct team_handle *th, uint32_t ifindex,
 int ifinfo_link(struct team_handle *th, uint32_t ifindex,
 		struct team_ifinfo **p_ifinfo);
 void ifinfo_unlink(struct team_ifinfo *ifinfo);
+void ifinfo_clear_changed(struct team_handle *th);
+void ifinfo_destroy_removed(struct team_handle *th);
+int get_ifinfo_list(struct team_handle *th);
 int get_options_handler(struct nl_msg *msg, void *arg);
 int option_list_alloc(struct team_handle *th);
 int option_list_init(struct team_handle *th);

commit 05a7a9598fc9e616a1acf19096aeb93f7288bb35

libteam: don't crash when trying to print unregistered device name
team_port_str() will crash when trying to print port name that was
just unregistered, if dellink event is handled before port removal
event.

This is regression from Commit 046fb6ba0aec ("libteam: resynchronize
ifinfo after lost RTNLGRP_LINK notifications"), which made it free
all removed interfaces after ifinfo handlers are called.

Put the ifinfo_destroy_removed() back to dellink/newlink handlers as
it was before that commit. Clean up the ifinfo list after change handlers
only if it refreshed the entire ifinfo list after lost events.

There's still a rare possibility that dellink event is missed due to
full socket receive buffer, which would cause ifinfo refresh and clearing
removed interfaces. For this, add NULL check to team_port_str() so it
doesn't try to print port device name in this situation.

Signed-off-by: Antti Tiainen <atiainen@forcepoint.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

include/team.h
@@ -223,6 +223,7 @@ enum {
 	TEAM_PORT_CHANGE	= 0x1,
 	TEAM_OPTION_CHANGE	= 0x2,
 	TEAM_IFINFO_CHANGE	= 0x4,
+	TEAM_IFINFO_REFRESH	= 0x8,
 	TEAM_ANY_CHANGE		= TEAM_PORT_CHANGE |
 				  TEAM_OPTION_CHANGE |
 				  TEAM_IFINFO_CHANGE,

libteam/ifinfo.c
@@ -258,6 +258,8 @@ static void obj_input_newlink(struct nl_object *obj, void *arg, bool event)
 	uint32_t ifindex;
 	int err;
 
+	ifinfo_destroy_removed(th);
+
 	link = (struct rtnl_link *) obj;
 
 	ifindex = rtnl_link_get_ifindex(link);
@@ -293,6 +295,8 @@ static void event_handler_obj_input_dellink(struct nl_object *obj, void *arg)
 	struct team_ifinfo *ifinfo;
 	uint32_t ifindex;
 	int err;
+
+	ifinfo_destroy_removed(th);
 
 	link = (struct rtnl_link *) obj;
 
@@ -412,7 +416,8 @@ int get_ifinfo_list(struct team_handle *th)
 		}
 	}
 
-	ret = check_call_change_handlers(th, TEAM_IFINFO_CHANGE);
+	ret = check_call_change_handlers(th, TEAM_IFINFO_CHANGE |
+					     TEAM_IFINFO_REFRESH);
 	if (ret < 0)
 		err(th, "get_ifinfo_list: check_call_change_handers failed");
 	return ret;

libteam/libteam.c
@@ -236,7 +236,7 @@ int check_call_change_handlers(struct team_handle *th,
 				break;
 		}
 	}
-	if (call_type_mask & TEAM_IFINFO_CHANGE) {
+	if (call_type_mask & TEAM_IFINFO_REFRESH) {
 		ifinfo_destroy_removed(th);
 		ifinfo_clear_changed(th);
 	}

libteam/stringify.c
@@ -344,7 +344,8 @@ static bool __team_port_str(struct team_port *port,
 			    team_is_port_removed(port) ? "-" :
 				team_is_port_changed(port) ? "*" : " ",
 			    ifindex,
-			    team_get_ifinfo_ifname(ifinfo),
+			    ifinfo ? team_get_ifinfo_ifname(ifinfo) :
+				"(removed)",
 			    team_is_port_link_up(port) ? "up": "down",
 			    team_get_port_speed(port),
 			    team_get_port_duplex(port) ? "FD" : "HD");