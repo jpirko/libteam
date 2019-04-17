jpirko/libteam

commit 6c00aaf0255322ffeb01b663c1f22f132db15699

teamd: add support for state item write operation
Signed-off-by: Jiri Pirko <jiri@resnulli.us>

teamd/teamd_state.c
@@ -19,6 +19,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
@@ -171,6 +172,8 @@ void teamd_state_val_unregister(struct teamd_context *ctx,
 	__unreg_val(ctx, val, priv, NULL);
 }
 
+#define TEAMD_STATE_PER_PORT_PREFIX "ports."
+
 static int teamd_state_build_val_json_subpath(json_t **p_vg_json_obj,
 					      json_t *root_json_obj,
 					      struct teamd_port *tdport,
@@ -182,7 +185,8 @@ static int teamd_state_build_val_json_subpath(json_t **p_vg_json_obj,
 	char *dot;
 
 	if (tdport)
-		ret = asprintf(&path, "$.ports.%s%s", tdport->ifname, subpath);
+		ret = asprintf(&path, "$." TEAMD_STATE_PER_PORT_PREFIX "%s%s",
+			       tdport->ifname, subpath);
 	else
 		ret = asprintf(&path, "$%s", subpath);
 	if (ret == -1)
@@ -270,6 +274,116 @@ static int teamd_state_vals_dump(struct teamd_context *ctx,
 		}
 	}
 	return 0;
+}
+
+static int __find_by_item_path(struct teamd_state_val_item **p_item,
+			       struct teamd_port **p_tdport,
+			       struct teamd_context *ctx, const char *item_path)
+{
+	struct teamd_state_val_item *item;
+	char *subpath;
+
+	if (!strncmp(item_path, TEAMD_STATE_PER_PORT_PREFIX,
+		     strlen(TEAMD_STATE_PER_PORT_PREFIX))) {
+		struct teamd_port *tdport;
+		bool found;
+		char *ifname_start = strchr(item_path, '.') + 1;
+		char *ifname_end = strchr(ifname_start, '.');
+		size_t ifname_len = ifname_end - ifname_start;
+
+		if (!ifname_end)
+			return -EINVAL;
+		subpath = ifname_end + 1;
+
+		found = false;
+		teamd_for_each_tdport(tdport, ctx) {
+			if (!strncmp(tdport->ifname, ifname_start,
+				     ifname_len)) {
+				*p_tdport = tdport;
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+			return -ENOENT;
+	} else {
+		subpath = (char *) item_path;
+		*p_tdport = NULL;
+	}
+
+	list_for_each_node_entry(item, &ctx->state_val_list, list) {
+		/* item->subpath[0] == '.' */
+		if (!strcmp(item->subpath + 1, subpath)) {
+			*p_item = item;
+			return 0;
+		}
+	}
+	return -ENOENT;
+}
+
+int __set_int_val(struct team_state_gsc *gsc, const char *value)
+{
+	long val;
+	char *endptr;
+
+	errno = 0;
+	val = strtol(value, &endptr, 10);
+	if (errno)
+		return -errno;
+	if (strlen(endptr) != 0)
+		return -EINVAL;
+	if (val < INT_MIN || val > INT_MAX)
+		return -ERANGE;
+	gsc->data.int_val = val;
+	return 0;
+}
+
+int __set_bool_val(struct team_state_gsc *gsc, const char *value)
+{
+	if (!strcasecmp("true", value))
+		gsc->data.bool_val = true;
+	else if (!strcasecmp("false", value))
+		gsc->data.bool_val = false;
+	else
+		return -EINVAL;
+	return 0;
+}
+
+int teamd_state_item_value_set(struct teamd_context *ctx, const char *item_path,
+			       const char *value)
+{
+	struct teamd_state_val_item *item;
+	const struct teamd_state_val *val;
+	void *priv;
+	struct team_state_gsc gsc;
+	int err;
+
+	err = __find_by_item_path(&item, &gsc.info.tdport, ctx, item_path);
+	if (err)
+		return err;
+
+	val = item->val;
+	priv = item->priv;
+	if (!val->setter)
+		return -EOPNOTSUPP;
+	switch (val->type) {
+	case TEAMD_STATE_ITEM_TYPE_INT:
+		err = __set_int_val(&gsc, value);
+		if (err)
+			return err;
+		break;
+	case TEAMD_STATE_ITEM_TYPE_STRING:
+		gsc.data.str_val.ptr = value;
+		break;
+	case TEAMD_STATE_ITEM_TYPE_BOOL:
+		err = __set_bool_val(&gsc, value);
+		if (err)
+			return err;
+		break;
+	case TEAMD_STATE_ITEM_TYPE_NODE:
+		TEAMD_BUG();
+	}
+	return val->setter(ctx, &gsc, priv);
 }
 
 struct state_ops_item {

include/teamdctl.h
@@ -54,6 +54,8 @@ int teamdctl_port_config_update_raw(struct teamdctl *tdc,
 char *teamdctl_config_get_raw(struct teamdctl *tdc);
 char *teamdctl_config_actual_get_raw(struct teamdctl *tdc);
 char *teamdctl_state_get_raw(struct teamdctl *tdc);
+int teamdctl_state_item_value_set(struct teamdctl *tdc, const char *item_path,
+				  const char *value);
 
 #ifdef __cplusplus
 } /* extern "C" */

libteamdctl/libteamdctl.c
@@ -417,3 +417,21 @@ char *teamdctl_state_get_raw(struct teamdctl *tdc)
 {
 	return tdc->cached_reply.state;
 }
+
+/**
+ * teamdctl_state_item_value_set:
+ * @tdc: libteamdctl library context
+ * @item_path: path to item
+ * @value: new value to be set
+ *
+ * Set state item value.
+ *
+ * Returns: zero on success or negative number in case of an error.
+ **/
+TEAMDCTL_EXPORT
+int teamdctl_state_item_value_set(struct teamdctl *tdc, const char *item_path,
+				  const char *value)
+{
+	return cli_method_call(tdc, "StateItemValueSet", NULL,
+			       "ss", item_path, value);
+}

teamd/teamd_ctl_methods.c
@@ -158,6 +158,34 @@ static int teamd_ctl_method_state_dump(struct teamd_context *ctx,
 	return err;
 }
 
+static int teamd_ctl_method_state_item_value_set(struct teamd_context *ctx,
+						 const struct teamd_ctl_method_ops *ops,
+						 void *ops_priv)
+{
+	const char *item_path;
+	const char *value;
+	int err;
+
+	err = ops->get_args(ops_priv, "ss", &item_path, &value);
+	if (err)
+		return ops->reply_err(ops_priv, "InvalidArgs", "Did not receive correct message arguments.");
+	teamd_log_dbgx(ctx, 2, "item_path \"%s\", value \"%s\"",
+		       item_path, value);
+
+	err = teamd_state_item_value_set(ctx, item_path, value);
+	if (err == -ENOENT) {
+		teamd_log_err("Failed to set state item \"%s\".", item_path);
+		return ops->reply_err(ops_priv, "PathDoesNotExist", "Item path does not exist.");
+	} else if (err == -EOPNOTSUPP) {
+		teamd_log_err("Failed to set state item \"%s\".", item_path);
+		return ops->reply_err(ops_priv, "OpNotSupp", "Operation not supported.");
+	} else if (err) {
+		teamd_log_err("Failed to set state item \"%s\".", item_path);
+		return ops->reply_err(ops_priv, "ItemValueSetFail", "Failed to set item value.");
+	}
+	return ops->reply_succ(ops_priv, NULL);
+}
+
 typedef int (*teamd_ctl_method_func_t)(struct teamd_context *ctx,
 				       const struct teamd_ctl_method_ops *ops,
 				       void *ops_priv);
@@ -195,6 +223,11 @@ static const struct teamd_ctl_method teamd_ctl_method_list[] = {
 	{
 		.name = "StateDump",
 		.func = teamd_ctl_method_state_dump,
+
+	},
+	{
+		.name = "StateItemValueSet",
+		.func = teamd_ctl_method_state_item_value_set,
 
 	},
 };

teamd/teamd_state.h
@@ -83,6 +83,8 @@ void teamd_state_ops_unregister(struct teamd_context *ctx,
 				const struct teamd_state_ops *ops,
 				void *priv);
 int teamd_state_dump(struct teamd_context *ctx, char **p_state_dump);
+int teamd_state_item_value_set(struct teamd_context *ctx, const char *item_path,
+			       const char *value);
 
 int teamd_state_basics_init(struct teamd_context *ctx);
 void teamd_state_basics_fini(struct teamd_context *ctx);

utils/teamdctl.c
@@ -617,6 +617,12 @@ static int call_method_port_config_dump(struct teamdctl *tdc,
 					   argv[0]);
 }
 
+static int call_method_state_item_set(struct teamdctl *tdc,
+				      int argc, char **argv)
+{
+	return teamdctl_state_item_value_set(tdc, argv[0], argv[1]);
+}
+
 
 enum id_command_type {
 	ID_CMDTYPE_NONE = 0,
@@ -627,6 +633,8 @@ enum id_command_type {
 	ID_CMDTYPE_S,
 	ID_CMDTYPE_S_D,
 	ID_CMDTYPE_S_V,
+	ID_CMDTYPE_S_I,
+	ID_CMDTYPE_S_I_S,
 	ID_CMDTYPE_P,
 	ID_CMDTYPE_P_A,
 	ID_CMDTYPE_P_R,
@@ -689,6 +697,18 @@ static struct command_type command_types[] = {
 		.parent_id = ID_CMDTYPE_S,
 		.name = "view",
 		.call_method = call_method_state_stateview,
+	},
+	{
+		.id = ID_CMDTYPE_S_I,
+		.parent_id = ID_CMDTYPE_S,
+		.name = "item",
+	},
+	{
+		.id = ID_CMDTYPE_S_I_S,
+		.parent_id = ID_CMDTYPE_S_I,
+		.name = "set",
+		.call_method = call_method_state_item_set,
+		.params = {"ITEMPATH", "VALUE"},
 	},
 	{
 		.id = ID_CMDTYPE_P,

commit 4dc3a7a042c88193f0371a33f1043919843e6447

teamd: tdport has to exist if item->per_port is set in __find_by_item_path
The issue can be reproduced by:

  # teamd -c '{"device":"team0", "runner":{"name":"lacp"}}' &
  # teamdctl team0 state item set runner.aggregator.selected true

teamd process will abort in lacp_port_state_aggregator_selected_set()
as gsc->info.tdport was set to NULL in teamd_state_item_value_set()

The item 'runner.aggregator.selected' is of per_port = true, and it
shouldn't allow to call its setter/getter().

This patch is to add the check for it in __find_by_item_path() called
by teamd_state_item_value_get/set().

Fixes: 6c00aaf02553 ("teamd: add support for state item write operation")
Signed-off-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd_state.c
@@ -333,6 +333,7 @@ static int __find_by_item_path(struct teamd_state_val_item **p_item,
 	list_for_each_node_entry(item, &ctx->state_val_list, list) {
 		/* item->subpath[0] == '.' */
 		if (!strcmp(item->subpath + 1, subpath) &&
+		    (!item->per_port || tdport) &&
 		    (!item->tdport || item->tdport == tdport)) {
 			*p_item = item;
 			*p_tdport = tdport;