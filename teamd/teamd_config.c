jpirko/libteam

commit c8b356a3cd363af10d71e21a4fb7dc26cf90b5bc

teamd: config: update local prio to kernel
Team port's priority will affect the active port selection. Update the
local config is not enough. We also need to update kernel configs.

Reported-by: Liang Li <liali@redhat.com>
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd_config.c
@@ -155,6 +155,31 @@ errout:
 	return err;
 }
 
+static int teamd_config_port_set(struct teamd_context *ctx, const char *port_name,
+				 json_t *port_obj)
+{
+	struct teamd_port *tdport;
+	json_t *config;
+	int tmp, err;
+
+	tdport = teamd_get_port_by_ifname(ctx, port_name);
+	if (!tdport)
+		return -ENODEV;
+
+	config = json_object_get(port_obj, "prio");
+	if (json_is_integer(config)) {
+		tmp = json_integer_value(config);
+		err = team_set_port_priority(ctx->th, tdport->ifindex, tmp);
+		if (err) {
+			teamd_log_err("%s: Failed to set \"priority\".",
+				      tdport->ifname);
+			return err;
+		}
+	}
+
+	return 0;
+}
+
 int teamd_config_port_update(struct teamd_context *ctx, const char *port_name,
 			     const char *json_port_cfg_str)
 {
@@ -184,6 +209,13 @@ int teamd_config_port_update(struct teamd_context *ctx, const char *port_name,
 	if (err)
 		teamd_log_err("%s: Failed to update existing config "
 			      "port object", port_name);
+	else {
+		err = teamd_config_port_set(ctx, port_name, port_new_obj);
+		if (err)
+			teamd_log_err("%s: Failed to update config to kernel",
+				      port_name);
+	}
+
 new_port_decref:
 	json_decref(port_new_obj);
 	return err;

commit 2bf8652cce4303d3ae79dff8bdef10dfae27439b

teamd: return 0 if tdport doesn't exist in teamd_config_port_set
This issue can be reproduced by doing:

  # ip link add dummy1 type dummy
  # teamd -t team0 -c '{"runner": {"name": "activebackup"}}' -d
  # teamdctl team0 port config update dummy1 '{"prio": -10}'
  # ip link set dummy0 master team0

and the error shows:

  libteamdctl: usock: Error message received: "ConfigUpdateFail"
  libteamdctl: usock: Error message content: "Failed to update config."
  command call failed (Invalid argument)

It's a regression caused by Commit c8b356a3cd36 ("teamd: config: update
local prio to kernel") where it requires the tdport has to exist when
a tdport config is being updated. However teamd supports for the port
config going first before the port being enslaved.

This issue breaks how NM-team starts a team device. Here to fix it by
returning 0 even if the tdport doesn't exist in teamd_config_port_set.

Reported-by: Radek Vykydal <rvykydal@redhat.com>
Fixes: c8b356a3cd36 ("teamd: config: update local prio to kernel")
Signed-off-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd_config.c
@@ -164,7 +164,7 @@ static int teamd_config_port_set(struct teamd_context *ctx, const char *port_nam
 
 	tdport = teamd_get_port_by_ifname(ctx, port_name);
 	if (!tdport)
-		return -ENODEV;
+		return 0;
 
 	config = json_object_get(port_obj, "prio");
 	if (json_is_integer(config)) {