jpirko/libteam

commit f32310b9a5cc0322d8f5c85d94e3866326bc68ce

libteam: wapper teamd_log_dbg with teamd_log_dbgx
Recently some users reported that they start to see debug messages in their
syslogs even with daemon_verbosity_level = LOG_INFO and without -g option.

Actually this issue is there at the begining, the user would see the debug
messages if they run teamd with -d option. The reason that most users did
not notice this is because they are using libteam via NetworkManager, and
NetworkManager run libteam in frontend.

But after commit e47d5db53873 ("teamd: add an option to force log
output to stdout, stderr or syslog"), NetworkManager will set
TEAM_LOG_OUTPUT=syslog in the environment. At the same time libdaemon
does not filter log levels if we use syslog(see function daemon_logv in
libdaemon). Then all the users would see the debug messages suddenly and
feels annoying.

And here is the quote for daemon_set_verbosity() from libdaemon/dlog.h
"""
Allows to decide which messages to output on standard output/error
streams. All messages are logged to syslog and this setting does
not influence that.
"""

Since we should not limit how our user(NM) used libteam. And libdaemon
is intend to not filter logs if use syslog. We'd better filter the
debug message ourselves, like via -g option. So I would prefer to
move all teamd_log_dbg to teamd_log_dbgx. After that, the user could
decide whether to enable debug or not by themselves with -g option.

Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd.h
@@ -49,15 +49,16 @@
 #define teamd_log_err(args...) daemon_log(LOG_ERR, ##args)
 #define teamd_log_warn(args...) daemon_log(LOG_WARNING, ##args)
 #define teamd_log_info(args...) daemon_log(LOG_INFO, ##args)
-#define teamd_log_dbg(args...) daemon_log(LOG_DEBUG, ##args)
 
 #define teamd_log_dbgx(ctx, val, args...)	\
 	if (val <= ctx->debug)			\
 		daemon_log(LOG_DEBUG, ##args)
 
+#define teamd_log_dbg(ctx, args...) teamd_log_dbgx(ctx, 1, ##args)
+
 static inline void TEAMD_BUG(void)
 {
-	teamd_log_dbg("BUG: %s:%d\n", __FILE__, __LINE__);
+	daemon_log(LOG_DEBUG, "BUG: %s:%d\n", __FILE__, __LINE__);
 	assert(0);
 }
 

teamd/teamd.c
@@ -332,7 +332,7 @@ static int teamd_run_loop_do_callbacks(struct list_item *lcb_list, fd_set *fds,
 			if (err) {
 				teamd_log_warn("Loop callback failed with: %s",
 					       strerror(-err));
-				teamd_log_dbg("Failed loop callback: %s, %p",
+				teamd_log_dbg(ctx, "Failed loop callback: %s, %p",
 					      lcb->name, lcb->priv);
 			}
 		}
@@ -519,7 +519,7 @@ static int __teamd_loop_callback_fd_add(struct teamd_context *ctx,
 		list_add_tail(&ctx->run_loop.callback_list, &lcb->list);
 	else
 		list_add(&ctx->run_loop.callback_list, &lcb->list);
-	teamd_log_dbg("Added loop callback: %s, %p", lcb->name, lcb->priv);
+	teamd_log_dbg(ctx, "Added loop callback: %s, %p", lcb->name, lcb->priv);
 	return 0;
 
 lcb_free:
@@ -636,7 +636,7 @@ void teamd_loop_callback_del(struct teamd_context *ctx, const char *cb_name,
 		list_del(&lcb->list);
 		if (lcb->is_period)
 			close(lcb->fd);
-		teamd_log_dbg("Removed loop callback: %s, %p",
+		teamd_log_dbg(ctx, "Removed loop callback: %s, %p",
 			      lcb->name, lcb->priv);
 		free(lcb->name);
 		free(lcb);
@@ -645,7 +645,7 @@ void teamd_loop_callback_del(struct teamd_context *ctx, const char *cb_name,
 	if (found)
 		teamd_run_loop_restart(ctx);
 	else
-		teamd_log_dbg("Callback named \"%s\" not found.", cb_name);
+		teamd_log_dbg(ctx, "Callback named \"%s\" not found.", cb_name);
 }
 
 int teamd_loop_callback_enable(struct teamd_context *ctx, const char *cb_name,
@@ -820,7 +820,7 @@ static int teamd_set_hwaddr(struct teamd_context *ctx)
 	if (err)
 		return 0; /* addr is not defined in config, no change needed */
 
-	teamd_log_dbg("Hwaddr string: \"%s\".", hwaddr_str);
+	teamd_log_dbg(ctx, "Hwaddr string: \"%s\".", hwaddr_str);
 	err = parse_hwaddr(hwaddr_str, &hwaddr, &hwaddr_len);
 	if (err) {
 		teamd_log_err("Failed to parse hardware address.");
@@ -953,16 +953,16 @@ static int teamd_runner_init(struct teamd_context *ctx)
 
 	err = teamd_config_string_get(ctx, &runner_name, "$.runner.name");
 	if (err) {
-		teamd_log_dbg("Failed to get team runner name from config.");
+		teamd_log_dbg(ctx, "Failed to get team runner name from config.");
 		runner_name = TEAMD_DEFAULT_RUNNER_NAME;
 		err = teamd_config_string_set(ctx, runner_name, "$.runner.name");
 		if (err) {
 			teamd_log_err("Failed to set default team runner name in config.");
 			return err;
 		}
-		teamd_log_dbg("Using default team runner \"%s\".", runner_name);
+		teamd_log_dbg(ctx, "Using default team runner \"%s\".", runner_name);
 	} else {
-		teamd_log_dbg("Using team runner \"%s\".", runner_name);
+		teamd_log_dbg(ctx, "Using team runner \"%s\".", runner_name);
 	}
 	ctx->runner = teamd_find_runner(runner_name);
 	if (!ctx->runner) {
@@ -1108,12 +1108,12 @@ static void debug_log_port_list(struct teamd_context *ctx)
 	char buf[120];
 	bool trunc;
 
-	teamd_log_dbg("<port_list>");
+	teamd_log_dbg(ctx, "<port_list>");
 	team_for_each_port(port, ctx->th) {
 		trunc = team_port_str(port, buf, sizeof(buf));
-		teamd_log_dbg("%s %s", buf, trunc ? "<trunc>" : "");
+		teamd_log_dbg(ctx, "%s %s", buf, trunc ? "<trunc>" : "");
 	}
-	teamd_log_dbg("</port_list>");
+	teamd_log_dbg(ctx, "</port_list>");
 }
 
 static void debug_log_option_list(struct teamd_context *ctx)
@@ -1139,12 +1139,12 @@ static void debug_log_ifinfo_list(struct teamd_context *ctx)
 	char buf[120];
 	bool trunc;
 
-	teamd_log_dbg("<ifinfo_list>");
+	teamd_log_dbg(ctx, "<ifinfo_list>");
 	team_for_each_ifinfo(ifinfo, ctx->th) {
 		trunc = team_ifinfo_str(ifinfo, buf, sizeof(buf));
-		teamd_log_dbg("%s %s", buf, trunc ? "<trunc>" : "");
+		teamd_log_dbg(ctx, "%s %s", buf, trunc ? "<trunc>" : "");
 	}
-	teamd_log_dbg("</ifinfo_list>");
+	teamd_log_dbg(ctx, "</ifinfo_list>");
 }
 
 static int debug_change_handler_func(struct team_handle *th, void *priv,
@@ -1573,7 +1573,7 @@ static int teamd_generate_devname(struct teamd_context *ctx)
 		if (err)
 			return err;
 	} while (ifindex);
-	teamd_log_dbg("Generated team device name \"%s\".", buf);
+	teamd_log_dbg(ctx, "Generated team device name \"%s\".", buf);
 
 	ctx->team_devname = strdup(buf);
 	if (!ctx->team_devname)
@@ -1597,7 +1597,7 @@ static int teamd_get_devname(struct teamd_context *ctx, bool generate_enabled)
 			}
 			goto skip_set;
 		} else {
-			teamd_log_dbg("Failed to get team device name from config.");
+			teamd_log_dbg(ctx, "Failed to get team device name from config.");
 			if (generate_enabled) {
 				err = teamd_generate_devname(ctx);
 				if (err) {
@@ -1617,7 +1617,7 @@ static int teamd_get_devname(struct teamd_context *ctx, bool generate_enabled)
 	}
 
 skip_set:
-	teamd_log_dbg("Using team device \"%s\".", ctx->team_devname);
+	teamd_log_dbg(ctx, "Using team device \"%s\".", ctx->team_devname);
 
 	err = asprintf(&ctx->ident, "%s_%s", ctx->argv0, ctx->team_devname);
 	if (err == -1) {
@@ -1835,9 +1835,9 @@ int main(int argc, char **argv)
 	daemon_log_ident = ctx->ident;
 	daemon_pid_file_proc = teamd_pid_file_proc;
 
-	teamd_log_dbg("Using PID file \"%s\"", daemon_pid_file_proc());
+	teamd_log_dbg(ctx, "Using PID file \"%s\"", daemon_pid_file_proc());
 	if (ctx->config_file)
-		teamd_log_dbg("Using config file \"%s\"", ctx->config_file);
+		teamd_log_dbg(ctx, "Using config file \"%s\"", ctx->config_file);
 
 	switch (ctx->cmd) {
 	case DAEMON_CMD_HELP:

teamd/teamd_balancer.c
@@ -174,12 +174,14 @@ static void tb_clear_rebalance_data(struct teamd_balancer *tb)
 	}
 }
 
-static int tb_hash_to_port_remap(struct team_handle *th,
+static int tb_hash_to_port_remap(struct teamd_balancer *tb,
+				 struct team_handle *th,
 				 struct tb_hash_info *tbhi,
 				 struct tb_port_info *tbpi)
 {
 	struct team_option *option;
 	struct teamd_port *new_tdport = tbpi->tdport;
+	struct teamd_context *ctx = tb->ctx;
 	uint8_t hash = tbhi->hash;
 	int err;
 
@@ -192,7 +194,7 @@ static int tb_hash_to_port_remap(struct team_handle *th,
 	err = team_set_option_value_u32(th, option, new_tdport->ifindex);
 	if (err)
 		return err;
-	teamd_log_dbg("Remapped hash \"%u\" (delta %" PRIu64 ") to port %s.",
+	teamd_log_dbg(ctx, "Remapped hash \"%u\" (delta %" PRIu64 ") to port %s.",
 		      hash, tb_stats_get_delta(&tbhi->stats),
 		      new_tdport->ifname);
 	return 0;
@@ -203,6 +205,7 @@ static int tb_rebalance(struct teamd_balancer *tb, struct team_handle *th)
 	int err;
 	struct tb_hash_info *tbhi;
 	struct tb_port_info *tbpi;
+	struct teamd_context *ctx = tb->ctx;
 
 	if (!tb->tx_balancing_enabled)
 		return 0;
@@ -216,7 +219,7 @@ static int tb_rebalance(struct teamd_balancer *tb, struct team_handle *th)
 			tbhi->rebalance.processed = true;
 			continue;
 		}
-		err = tb_hash_to_port_remap(th, tbhi, tbpi);
+		err = tb_hash_to_port_remap(tb, th, tbhi, tbpi);
 		if (err) {
 			tbpi->rebalance.unusable = true;
 			continue;
@@ -228,7 +231,7 @@ static int tb_rebalance(struct teamd_balancer *tb, struct team_handle *th)
 	list_for_each_node_entry(tbpi, &tb->port_info_list, list) {
 		if (tbpi->rebalance.unusable)
 			continue;
-		teamd_log_dbg("Port %s rebalanced, delta: %" PRIu64,
+		teamd_log_dbg(ctx, "Port %s rebalanced, delta: %" PRIu64,
 			      tbpi->tdport->ifname, tbpi->rebalance.bytes);
 	}
 	return 0;
@@ -303,7 +306,7 @@ static int tb_option_change_handler_func(struct team_handle *th, void *priv,
 					      array_index);
 				return -EINVAL;
 			}
-			teamd_log_dbg("stats update for hash \"%u\": \"%" PRIu64 "\".",
+			teamd_log_dbg(ctx, "stats update for hash \"%u\": \"%" PRIu64 "\".",
 				      array_index, lb_stats->tx_bytes);
 			tb_stats_update_hash(tb, array_index,
 					     lb_stats->tx_bytes);
@@ -319,7 +322,7 @@ static int tb_option_change_handler_func(struct team_handle *th, void *priv,
 					      port_ifindex);
 				return -EINVAL;
 			}
-			teamd_log_dbg("stats update for port %s: \"%" PRIu64 "\".",
+			teamd_log_dbg(ctx, "stats update for port %s: \"%" PRIu64 "\".",
 				      tdport->ifname, lb_stats->tx_bytes);
 			tb_stats_update_port(tb, tdport,
 					     lb_stats->tx_bytes);

teamd/teamd_dbus.c
@@ -178,7 +178,7 @@ static DBusHandlerResult message_handler(DBusConnection *con,
 	msg_interface = dbus_message_get_interface(message);
 	if (!method || !path || !msg_interface)
 		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-	teamd_log_dbg("dbus: %s.%s (%s)", msg_interface, method, path);
+	teamd_log_dbg(ctx, "dbus: %s.%s (%s)", msg_interface, method, path);
 
 	if (!strcmp(method, "Introspect") &&
 	    !strcmp(msg_interface, "org.freedesktop.DBus.Introspectable")) {
@@ -504,7 +504,7 @@ int teamd_dbus_init(struct teamd_context *ctx)
 	if (err)
 		goto iface_fini;
 	id = dbus_connection_get_server_id(ctx->dbus.con),
-	teamd_log_dbg("dbus: connected to %s with name %s", id,
+	teamd_log_dbg(ctx, "dbus: connected to %s with name %s", id,
 		      dbus_bus_get_unique_name(ctx->dbus.con));
 	dbus_free(id);
 	return 0;
@@ -541,7 +541,7 @@ int teamd_dbus_expose_name(struct teamd_context *ctx)
 	err = dbus_bus_request_name(ctx->dbus.con, service_name, 0,
 				    &error);
 	if (err == DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER) {
-		teamd_log_dbg("dbus: have name %s", service_name);
+		teamd_log_dbg(ctx, "dbus: have name %s", service_name);
 		err = 0;
 	} else if (dbus_error_is_set(&error)) {
 		teamd_log_err("dbus: Failed to acquire %s: %s: %s",

teamd/teamd_hash_func.c
@@ -170,7 +170,7 @@ int teamd_hash_func_set(struct teamd_context *ctx)
 	int err;
 
 	if (!teamd_config_path_exists(ctx, "$.runner.tx_hash")) {
-		teamd_log_dbg("No Tx hash recipe found in config.");
+		teamd_log_dbg(ctx, "No Tx hash recipe found in config.");
 		err = teamd_hash_func_add_default_frags(ctx);
 		if (err)
 			return err;

teamd/teamd_link_watch.c
@@ -281,7 +281,7 @@ static int link_watch_load_config_one(struct teamd_context *ctx,
 
 	err = team_get_port_user_linkup(ctx->th, tdport->ifindex, &linkup);
 	if (!err) {
-		teamd_log_dbg("%s: Current user link state is \"%s\".",
+		teamd_log_dbg(ctx, "%s: Current user link state is \"%s\".",
 			      tdport->ifname, linkup ? "up" : "down");
 	}
 
@@ -351,7 +351,7 @@ static int link_watch_event_watch_port_added(struct teamd_context *ctx,
 	cpcookie = teamd_config_path_cookie_get(ctx, "$.ports.%s.link_watch",
 						tdport->ifname);
 	if (cpcookie) {
-		teamd_log_dbg("%s: Got link watch from port config.",
+		teamd_log_dbg(ctx, "%s: Got link watch from port config.",
 			      tdport->ifname);
 		err = link_watch_load_config(ctx, tdport, cpcookie);
 		if (err)
@@ -360,7 +360,7 @@ static int link_watch_event_watch_port_added(struct teamd_context *ctx,
 
 	cpcookie = teamd_config_path_cookie_get(ctx, "$.link_watch");
 	if (cpcookie) {
-		teamd_log_dbg("%s: Got link watch from global config.",
+		teamd_log_dbg(ctx, "%s: Got link watch from global config.",
 			      tdport->ifname);
 		err = link_watch_load_config(ctx, tdport, cpcookie);
 		if (err)
@@ -380,7 +380,7 @@ static int link_watch_event_watch_port_added(struct teamd_context *ctx,
 				      tdport->ifname);
 			return err;
 		}
-		teamd_log_dbg("%s: Using implicit link watch.", tdport->ifname);
+		teamd_log_dbg(ctx, "%s: Using implicit link watch.", tdport->ifname);
 		return link_watch_event_watch_port_added(ctx, tdport, priv);
 	}
 	return 0;

teamd/teamd_lw_arp_ping.c
@@ -178,7 +178,7 @@ static int lw_ap_load_options(struct teamd_context *ctx,
 	 * If source_host is not provided, just use address 0.0.0.0 according
 	 * to RFC 5227 (IPv4 Address Conflict Detection).
 	 */
-	teamd_log_dbg("source address \"%s\".",
+	teamd_log_dbg(ctx, "source address \"%s\".",
 		      str_in_addr(&ap_ppriv->src));
 
 	err = teamd_config_string_get(ctx, &host, "@.target_host", cpcookie);
@@ -189,25 +189,25 @@ static int lw_ap_load_options(struct teamd_context *ctx,
 	err = set_in_addr(&ap_ppriv->dst, host);
 	if (err)
 		return err;
-	teamd_log_dbg("target address \"%s\".", str_in_addr(&ap_ppriv->dst));
+	teamd_log_dbg(ctx, "target address \"%s\".", str_in_addr(&ap_ppriv->dst));
 
 	err = teamd_config_bool_get(ctx, &ap_ppriv->validate_active,
 				    "@.validate_active", cpcookie);
 	if (err)
 		ap_ppriv->validate_active = false;
-	teamd_log_dbg("validate_active \"%d\".", ap_ppriv->validate_active);
+	teamd_log_dbg(ctx, "validate_active \"%d\".", ap_ppriv->validate_active);
 
 	err = teamd_config_bool_get(ctx, &ap_ppriv->validate_inactive,
 				    "@.validate_inactive", cpcookie);
 	if (err)
 		ap_ppriv->validate_inactive = false;
-	teamd_log_dbg("validate_inactive \"%d\".", ap_ppriv->validate_inactive);
+	teamd_log_dbg(ctx, "validate_inactive \"%d\".", ap_ppriv->validate_inactive);
 
 	err = teamd_config_bool_get(ctx, &ap_ppriv->send_always,
 				    "@.send_always", cpcookie);
 	if (err)
 		ap_ppriv->send_always = false;
-	teamd_log_dbg("send_always \"%d\".", ap_ppriv->send_always);
+	teamd_log_dbg(ctx, "send_always \"%d\".", ap_ppriv->send_always);
 
 	err = teamd_config_int_get(ctx, &tmp, "@.vlanid", cpcookie);
 	if (!err) {
@@ -217,7 +217,7 @@ static int lw_ap_load_options(struct teamd_context *ctx,
 		}
 		ap_ppriv->vlanid_in_use = true;
 		ap_ppriv->vlanid = tmp;
-		teamd_log_dbg("vlan id \"%u\".", ap_ppriv->vlanid);
+		teamd_log_dbg(ctx, "vlan id \"%u\".", ap_ppriv->vlanid);
 	}
 
 	return 0;

teamd/teamd_lw_ethtool.c
@@ -115,7 +115,7 @@ static int lw_ethtool_load_options(struct teamd_context *ctx,
 			teamd_log_err("\"delay_up\" must not be negative number.");
 			return -EINVAL;
 		}
-		teamd_log_dbg("delay_up \"%d\".", tmp);
+		teamd_log_dbg(ctx, "delay_up \"%d\".", tmp);
 		ms_to_timespec(&ethtool_ppriv->delay_up, tmp);
 	}
 	err = teamd_config_int_get(ctx, &tmp, "@.delay_down", cpcookie);
@@ -124,7 +124,7 @@ static int lw_ethtool_load_options(struct teamd_context *ctx,
 			teamd_log_err("\"delay_down\" must not be negative number.");
 			return -EINVAL;
 		}
-		teamd_log_dbg("delay_down \"%d\".", tmp);
+		teamd_log_dbg(ctx, "delay_down \"%d\".", tmp);
 		ms_to_timespec(&ethtool_ppriv->delay_down, tmp);
 	}
 	return 0;

commit 575edc0dbc7ae3dfa34e30c1496c3504eacbf56e

teamd: fix build error in expansion of macro teamd_log_dbgx
With gcc 8.3 I got the following build error:

In file included from teamd_dbus.c:33:
teamd_dbus.c: In function 'teamd_dbus_init':
teamd.h:54:2: error: expected expression before 'if'
  if (val <= ctx->debug)   \
  ^~
teamd.h:57:37: note: in expansion of macro 'teamd_log_dbgx'
 #define teamd_log_dbg(ctx, args...) teamd_log_dbgx(ctx, 1, ##args)
                                     ^~~~~~~~~~~~~~
teamd_dbus.c:507:2: note: in expansion of macro 'teamd_log_dbg'
  teamd_log_dbg(ctx, "dbus: connected to %s with name %s", id,
  ^~~~~~~~~~~~~

Fix it by adding parentheses and braces around the content.

Fixes: f32310b9a5cc ("libteam: wapper teamd_log_dbg with teamd_log_dbgx")
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd.h
@@ -51,8 +51,7 @@
 #define teamd_log_info(args...) daemon_log(LOG_INFO, ##args)
 
 #define teamd_log_dbgx(ctx, val, args...)	\
-	if (val <= ctx->debug)			\
-		daemon_log(LOG_DEBUG, ##args)
+	({ if (val <= ctx->debug) daemon_log(LOG_DEBUG, ##args); })
 
 #define teamd_log_dbg(ctx, args...) teamd_log_dbgx(ctx, 1, ##args)
 