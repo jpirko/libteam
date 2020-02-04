jpirko/libteam

commit 5f351666409ffe05f40012e5bf135807d4d0260c

teamd: add port_master_ifindex_changed for link_watch_port_watch_ops
This is a follow up of Commit b6f63db7f3c8 ("teamd: add
port_master_ifindex_changed for teamd_event_watch_ops")
to fix the same issue in it.

The issue is a race between option change event and ifinfo
(master_ifindex) change event, so wherever a option change
function tries to iterate tdport by teamd_for_each_tdport,
the same thing should be done in its master_ifindex change
function, including link_watch_port_watch_ops.

The fix has been verified in one customer's env where it
appeared no arp requests coming out on the only state-up
tdport prior to this patch.

Reported-by: Michal Tesar <mtesar@redhat.com>
Fixes: b6f63db7f3c8 ("teamd: add port_master_ifindex_changed for teamd_event_watch_ops")
Signed-off-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

teamd/teamd_link_watch.c
@@ -450,10 +450,19 @@ static int link_watch_enabled_option_changed(struct teamd_context *ctx,
 	return link_watch_refresh_forced_send(ctx);
 }
 
+
+static int link_watch_port_master_ifindex_changed(struct teamd_context *ctx,
+						  struct teamd_port *tdport,
+						  void *priv)
+{
+	return link_watch_refresh_forced_send(ctx);
+}
+
 static const struct teamd_event_watch_ops link_watch_port_watch_ops = {
 	.port_added = link_watch_event_watch_port_added,
 	.port_removed = link_watch_event_watch_port_removed,
 	.port_link_changed = link_watch_event_watch_port_link_changed,
+	.port_master_ifindex_changed = link_watch_port_master_ifindex_changed,
 	.option_changed = link_watch_enabled_option_changed,
 	.option_changed_match_name = "enabled",
 };

commit c7237377dead39ae4a711297203bacf7edb9fa41

teamd: fix possible race in master ifname callback
In teamd the messages from kernel are processed in order:
options
ifinfo
ports

However, kernel sends the messages in a different order. See:
team_upper_dev_link() which generates ifinfo notification
__team_port_change_port_added() which generates ports notification
__team_options_change_check() which generates options notification

So there is a chance that the teamd processed only the port without
options and right after that it processes ifinfo.

Fix this by introducing teamd_port_enabled_check() which does not log
error and ignore the port in case this call fails. This is safe
as this is going to be handled by future
link_watch_enabled_option_changed() call.

Signed-off-by: Jiri Pirko <jiri@mellanox.com>
Fixes: 5f351666409f ("teamd: add port_master_ifindex_changed for link_watch_port_watch_ops")
Tested-by: Stepan Blyshchak <stepanb@mellanox.com>
Reviewed-by: Xin Long <lucien.xin@gmail.com>

teamd/teamd.h
@@ -325,6 +325,8 @@ int teamd_port_remove_all(struct teamd_context *ctx);
 void teamd_port_obj_remove_all(struct teamd_context *ctx);
 int teamd_port_enabled(struct teamd_context *ctx, struct teamd_port *tdport,
 		       bool *enabled);
+int teamd_port_enabled_check(struct teamd_context *ctx,
+			     struct teamd_port *tdport, bool *enabled);
 int teamd_port_prio(struct teamd_context *ctx, struct teamd_port *tdport);
 int teamd_port_check_enable(struct teamd_context *ctx,
 			    struct teamd_port *tdport,

teamd/teamd_link_watch.c
@@ -423,9 +423,16 @@ static int link_watch_refresh_forced_send(struct teamd_context *ctx)
 	int err;
 
 	teamd_for_each_tdport(tdport, ctx) {
-		err = teamd_port_enabled(ctx, tdport, &port_enabled);
-		if (err)
-			return err;
+		err = teamd_port_enabled_check(ctx, tdport, &port_enabled);
+		if (err) {
+			/* Looks like the options are not ready for this port.
+			 * This can happen when called from
+			 * link_watch_port_master_ifindex_changed(). Skip this
+			 * for now, let it be handled by future call of
+			 * link_watch_enabled_option_changed().
+			 */
+			continue;
+		}
 		__set_forced_send_for_port(tdport, port_enabled);
 		if (port_enabled)
 			enabled_port_count++;

teamd/teamd_per_port.c
@@ -389,24 +389,38 @@ int teamd_port_remove_ifname(struct teamd_context *ctx, const char *port_name)
 	return teamd_port_remove(ctx, tdport);
 }
 
-int teamd_port_enabled(struct teamd_context *ctx, struct teamd_port *tdport,
-		       bool *enabled)
+int __teamd_port_enabled(struct teamd_context *ctx, struct teamd_port *tdport,
+			 bool *enabled, bool may_fail)
 {
 	struct team_option *option;
 
 	option = team_get_option(ctx->th, "np", "enabled", tdport->ifindex);
 	if (!option) {
-		teamd_log_err("%s: Failed to find \"enabled\" option.",
-			      tdport->ifname);
+		if (!may_fail)
+			teamd_log_err("%s: Failed to find \"enabled\" option.",
+				      tdport->ifname);
 		return -ENOENT;
 	}
 	if (team_get_option_type(option) != TEAM_OPTION_TYPE_BOOL) {
-		teamd_log_err("Unexpected type of \"enabled\" option.");
+		if (!may_fail)
+			teamd_log_err("Unexpected type of \"enabled\" option.");
 		return -EINVAL;
 	}
 
 	*enabled = team_get_option_value_bool(option);
 	return 0;
+}
+
+int teamd_port_enabled(struct teamd_context *ctx, struct teamd_port *tdport,
+		       bool *enabled)
+{
+	return __teamd_port_enabled(ctx, tdport, enabled, false);
+}
+
+int teamd_port_enabled_check(struct teamd_context *ctx,
+			     struct teamd_port *tdport, bool *enabled)
+{
+	return __teamd_port_enabled(ctx, tdport, enabled, true);
 }
 
 int teamd_port_prio(struct teamd_context *ctx, struct teamd_port *tdport)