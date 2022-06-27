jpirko/libteam

commit d8163e34c25c93c8dee6a7cba5f7cc9135bc340b

libteamdctl: do test method call instead or Introspect call
Signed-off-by: Jiri Pirko <jiri@resnulli.us>

libteamdctl/cli_dbus.c
@@ -169,58 +169,6 @@ out:
 	return err;
 }
 
-static int cli_dbus_introspect(struct teamdctl *tdc,
-			       struct cli_dbus_priv *cli_dbus)
-{
-	DBusMessage *msg;
-	dbus_bool_t dbres;
-	DBusPendingCall *pending;
-	int err;
-
-	msg = dbus_message_new_method_call(cli_dbus->service_name,
-					   TEAMD_DBUS_PATH,
-					   "org.freedesktop.DBus.Introspectable",
-					   "Introspect");
-	if (!msg) {
-		err(tdc, "Failed to create message.");
-		return -ENOMEM;
-	}
-
-	dbres = dbus_connection_send_with_reply(cli_dbus->conn, msg,
-						&pending, -1);
-	if (dbres == FALSE) {
-		err(tdc, "Send with reply failed.");
-		err = -ENOMEM;
-		goto free_msg;
-	}
-	if (!pending) {
-		err(tdc, "Pending call not created.");
-		err = -ENOMEM;
-		goto free_msg;
-	}
-
-	dbus_pending_call_block(pending);
-
-	dbus_message_unref(msg);
-	msg = dbus_pending_call_steal_reply(pending);
-	dbus_pending_call_unref(pending);
-	if (!msg) {
-		err(tdc, "Failed to get reply.");
-		err = -EINVAL;
-		goto out;
-	}
-
-	err = cli_dbus_check_error_msg(tdc, msg);
-	if (err)
-		goto free_msg;
-
-free_msg:
-	dbus_message_unref(msg);
-out:
-	return err;
-
-}
-
 static int cli_dbus_init(struct teamdctl *tdc, const char *team_name, void *priv)
 {
 	struct cli_dbus_priv *cli_dbus = priv;
@@ -241,13 +189,7 @@ static int cli_dbus_init(struct teamdctl *tdc, const char *team_name, void *priv
 		err = -EINVAL;
 		goto free_service_name;
 	}
-
-	/* Now, try to introspect to see if it's possible to call methods */
-	err = cli_dbus_introspect(tdc, cli_dbus);
-	if (err) {
-		err(tdc, "Failed to do introspection.");
-		goto free_service_name;
-	}
+	err = 0;
 	goto free_error;
 
 free_service_name:
@@ -269,6 +211,7 @@ static const struct teamdctl_cli cli_dbus = {
 	.name = "dbus",
 	.init = cli_dbus_init,
 	.fini = cli_dbus_fini,
+	.test_method_call_required = true,
 	.method_call = cli_dbus_method_call,
 	.priv_size = sizeof(struct cli_dbus_priv),
 };

libteamdctl/libteamdctl.c
@@ -191,6 +191,11 @@ static int cli_init(struct teamdctl *tdc, const char *team_name)
 	err = tdc->cli->init(tdc, team_name, tdc->cli_priv);
 	if (err)
 		goto free_priv;
+	if (tdc->cli->test_method_call_required) {
+		err = cli_method_call(tdc, "ConfigDump", NULL, "");
+		if (err)
+			goto free_priv;
+	}
 	return 0;
 free_priv:
 	if (tdc->cli->priv_size)

libteamdctl/teamdctl_private.h
@@ -20,6 +20,7 @@
 #ifndef _TEAMDCTL_PRIVATE_H_
 #define _TEAMDCTL_PRIVATE_H_
 
+#include <stdbool.h>
 #include <syslog.h>
 #include <teamdctl.h>
 
@@ -86,6 +87,7 @@ struct teamdctl_cli {
 	size_t priv_size;
 	int (*init)(struct teamdctl *tdc, const char *team_name, void *priv);
 	void (*fini)(struct teamdctl *tdc, void *priv);
+	bool test_method_call_required;
 	int (*method_call)(struct teamdctl *tdc, const char *method_name,
 			   char **p_reply, void *priv,
 			   const char *fmt, va_list ap);

commit 3bbce8a171deab6cd3d7d57d128bc2dbaea451f0

libteamdctl: validate the bus name before using it
Using bus name without validating it will cause core dump generated,
and it can be reproduced by:

  # ip link add dummy0.1 type dummy
  # teamdctl dummy0.1 state dump

  This is normally a bug in some application using the D-Bus library.

    D-Bus not built with -rdynamic so unable to print a backtrace
  Aborted (core dumped)

Doing this many times can even create too many core files, customers
may complain about it.

This is triggered when calling cli_method_call("ConfigDump") in
cli_init(), so fix it by returning err in cli->init/cli_dbus_init()
if the bus name fails to validate.

Note this is safe, as with dbus, we can't use invalid dbus name to
create the team dev either.

Fixes: d8163e34c25c ("libteamdctl: do test method call instead or Introspect call")
Reported-by: Uday Patel <upatel@redhat.com>
Signed-off-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@nvidia.com>

libteamdctl/cli_dbus.c
@@ -183,12 +183,17 @@ static int cli_dbus_init(struct teamdctl *tdc, const char *team_name, void *priv
 	if (ret == -1)
 		return -errno;
 
+	err = -EINVAL;
 	dbus_error_init(&error);
+	if (!dbus_validate_bus_name(cli_dbus->service_name, &error)) {
+		err(tdc, "dbus: Could not validate bus name: %s - %s",
+			 error.name, error.message);
+		goto free_service_name;
+	}
 	cli_dbus->conn = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
 	if (!cli_dbus->conn) {
 		err(tdc, "dbus: Could not acquire the system bus: %s - %s",
 			 error.name, error.message);
-		err = -EINVAL;
 		goto free_service_name;
 	}
 	err = 0;