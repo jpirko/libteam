jpirko/libteam

commit 8180763d6f82c6381828ef250de36979c7dfae67

libteam: always add newly created option to option list
Coverity error:
CID 1031790 (#1 of 1): Resource leak (RESOURCE_LEAK)5. leaked_storage:
Variable "option" going out of scope leaks the storage it points to.

Signed-off-by: Jiri Pirko <jiri@resnulli.us>

libteam/options.c
@@ -51,10 +51,12 @@ struct team_option {
 	int			data_len;
 	bool			changed;
 	bool			changed_locally;
+	bool			temporary;
 };
 
-static void free_option(struct team_option *option)
+static void destroy_option(struct team_option *option)
 {
+	list_del(&option->list);
 	free(option->id.name);
 	free(option->data);
 	free(option);
@@ -64,18 +66,19 @@ static void flush_option_list(struct team_handle *th)
 {
 	struct team_option *option, *tmp;
 
-	list_for_each_node_entry_safe(option, tmp, &th->option_list, list) {
-		list_del(&option->list);
-		free_option(option);
-	}
+	list_for_each_node_entry_safe(option, tmp, &th->option_list, list)
+		destroy_option(option);
 }
 
 static void option_list_cleanup_last_state(struct team_handle *th)
 {
-	struct team_option *option;
+	struct team_option *option, *tmp;
 
-	list_for_each_node_entry(option, &th->option_list, list)
+	list_for_each_node_entry_safe(option, tmp, &th->option_list, list) {
 		option->changed = false;
+		if (option->temporary)
+			destroy_option(option);
+	}
 }
 
 static struct team_option *do_find_option(struct team_handle *th,
@@ -120,7 +123,7 @@ static int get_option_data_size_by_type(int opt_type, const void *data,
 	}
 }
 
-static int create_option(struct team_option **poption,
+static int create_option(struct team_handle *th, struct team_option **poption,
 			 struct team_option_id *opt_id)
 {
 	struct team_option *option;
@@ -139,6 +142,8 @@ static int create_option(struct team_option **poption,
 	option->id.port_ifindex_used = opt_id->port_ifindex_used;
 	option->id.array_index = opt_id->array_index;
 	option->id.array_index_used = opt_id->array_index_used;
+
+	list_add(&th->option_list, &option->list);
 
 	*poption = option;
 	return 0;
@@ -191,7 +196,7 @@ static int update_option(struct team_handle *th, struct team_option **poption,
 
 	option = do_find_option(th, opt_id);
 	if (!option) {
-		err = create_option(&option, opt_id);
+		err = create_option(th, &option, opt_id);
 		if (err)
 			return err;
 		option_created = true;
@@ -200,11 +205,9 @@ static int update_option(struct team_handle *th, struct team_option **poption,
 			       changed, changed_locally);
 	if (err) {
 		if (option_created)
-			free_option(option);
+			destroy_option(option);
 		return err;
 	}
-	if (option_created)
-		list_add(&th->option_list, &option->list);
 	*poption = option;
 	return 0;
 }
@@ -318,10 +321,8 @@ int get_options_handler(struct nl_msg *msg, void *arg)
 			err(th, "Failed to update option: %s", strerror(-err));
 			continue;
 		}
-		if (option_attrs[TEAM_ATTR_OPTION_REMOVED]) {
-			list_del(&option->list);
-			free_option(option);
-		}
+		if (option_attrs[TEAM_ATTR_OPTION_REMOVED])
+			destroy_option(option);
 	}
 
 	set_call_change_handlers(th, TEAM_OPTION_CHANGE);
@@ -393,9 +394,10 @@ static struct team_option *find_option(struct team_handle *th,
 	 * In case option does not exist, create new uninitialized one
 	 * which can be used for option tracking.
 	 */
-	err = create_option(&option, opt_id);
+	err = create_option(th, &option, opt_id);
 	if (err)
 		return NULL;
+	option->temporary = true;
 	return option;
 }
 
@@ -682,6 +684,8 @@ static int local_set_option_value(struct team_handle *th,
 
 	err = update_option(th, &option, opt_id, opt_type,
 			    data, data_len, true, true);
+	if (option->temporary)
+		destroy_option(option);
 	if (err)
 		return err;
 	return 0;

commit 1569cf8ac96294083ed0031ef5b4174489cd9472

options: move option temporary check after the err check
option is set only if err returned from update_option() is 0, so
check err first before checking option->temporary. Otherwise, it
may results in a dereference of an undefined pointer value.

Fixes: 8180763d6f82 ("libteam: always add newly created option to option list")
Signed-off-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@nvidia.com>

libteam/options.c
@@ -688,10 +688,10 @@ static int local_set_option_value(struct team_handle *th,
 
 	err = update_option(th, &option, opt_id, opt_type,
 			    data, data_len, true, true);
-	if (option->temporary)
-		destroy_option(option);
 	if (err)
 		return err;
+	if (option->temporary)
+		destroy_option(option);
 	return 0;
 }
 